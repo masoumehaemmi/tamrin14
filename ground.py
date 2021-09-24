6b039930708fcafe2127261491d60b5b2035bb52
masoumeaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-24 13:58:33 +0330

    Add files via upload

---
+++ ground.py
@@ -0,0 +1,19 @@
+import arcade
+
+class Ground(arcade.AnimatedWalkingSprite):
+    def __init__(self,width, height):
+        super().__init__()
+
+        self.walk_left_textures = [arcade.load_texture("image\ground0.jpg"),
+                                   arcade.load_texture("image\ground1.jpg1"),
+                                   arcade.load_texture("image\ground2.jpg2")]
+        self.center_x = width
+        self.center_y = height
+
+        self.width = 50
+        self.height = 50
+
+        self.change_x = -4
+        self.change_y =0
+
+        self.speed = 4
