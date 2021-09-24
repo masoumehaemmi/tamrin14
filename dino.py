b7be85f9cad13d3cb8575b28fc67fd94ab50c24a
masoumeaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-24 23:25:48 +0330

    Add files via upload

--- dino.py
+++ dino.py
@@ -3,14 +3,16 @@ import arcade
 
 
 class Dino(arcade.Sprite):
-    def __init__(self, w, h):
+    def __init__(self, x, y):
         super().__init__()
 
-        self.texture= arcade.load_texture("image\dino g.jpg")
+        self.texture= arcade.load_texture("image\d1.png" ,mirrored= True)
 
-        self.width = 150
-        self.height = 150
-        self.center_x = w
-        self.center_y = h
+        self.width = 120
+        self.height = 120
+        self.center_x = 50
+        self.center_y = 200
+        # self.change_x = 0
+        self.score = 0
     
     
