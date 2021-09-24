b3ffbaa5e26757718d2b738e2509509384e657b2
masoumeaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-24 23:25:33 +0330

    Add files via upload

--- bird.py
+++ bird.py
@@ -4,8 +4,8 @@ import arcade
 class Bird(arcade.AnimatedWalkingSprite):
     def __init__(self, width, speed):
         super().__init__()
-        self.walk_left_textures = [arcade.load_texture('image\bird b0.jpg'),
-                                   arcade.load_texture('image\bird b1.jpg')]
+        self.walk_left_textures = [arcade.load_texture('image\drog0.png'),
+                                   arcade.load_texture('image\drog1.png')]
         self.center_x = width
         self.center_y = random.randint(250, 450)
         self.change_x = speed 
