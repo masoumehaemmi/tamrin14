5c2b0f504f346a7bb2e06e35bc15b734ae5a4008
masoumeaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-09-25 15:08:57 +0330

    Add files via upload

--- ground.py
+++ ground.py
@@ -1,19 +1,28 @@
+import random
 import arcade
 
-class Ground(arcade.AnimatedWalkingSprite):
-    def __init__(self,x, y):
+class Ground(arcade.Sprite):
+    def __init__(self):
         super().__init__()
 
-        self.walk_left_textures = [arcade.load_texture("image\g0.png"),
-                                   arcade.load_texture("image\g1.png"),
-                                   arcade.load_texture("image\g2.png")]
-        self.center_x = x
-        self.center_y = y
+        # self.walk_left_textures = [arcade.load_texture("image\g0.png"),
+        #                            arcade.load_texture("image\g1.png"),
+        #                            arcade.load_texture("image\g2.png")]
 
-        # self.width = 1000
-        # self.height = 100
+                                   
+        self.pic_path =random.choice(["image\g.png ", "image\wp4285370.png"]) 
+        self.texture = arcade.load_texture(self.pic_path)
+        self.center_x = 600
 
-        # self.change_x = -4
+        # self.center_y = 400
+
+        self.width = 1000
+        self.height = 100
+
+        self.change_x = -4
         # self.change_y =0
 
-        self.speed = 4
+        self.speed = 2
+
+    # def move(self, speed):
+    #     self.center_x += self.change_x * speed
